29369286a961d27c0a21680d282ebeb68cc84b35
saivarun4002 <91950083+saivarun4002@users.noreply.github.com>
2023-03-10 15:16:24 +0530

    Local Storage

---
+++ src/utils/fetchLocalStorage.js
@@ -0,0 +1,15 @@
+// support function to fetch all the local storage informations
+
+export const fetchUser = () => {
+    const userInfo = localStorage.getItem('user') !== "undefined" ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    return userInfo;
+}
+
+
+
+export const fetchCart = () => {
+    const cartInfo = localStorage.getItem('cartItems') !== "undefined" ? JSON.parse(localStorage.getItem('cartItems')) : localStorage.clear();
+
+    return cartInfo ? cartInfo: [];
+}
