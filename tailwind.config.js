eef8bfd294362f9dc8a2818b6e7c0bf67dc6590e
varun <91950083+saivarun4002@users.noreply.github.com>
2023-03-16 14:42:06 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,8 +57,7 @@ module.exports = {
         cartBg: "#282a2c",
         cartItem: "#2e3033",
         cartTotal: "#343739",
-        // bitch: "#2C74B3",
-        bitch: "#FF577F",
+        glitch: "#FF577F",
     
       },
     },
